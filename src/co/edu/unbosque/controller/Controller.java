5df9be27f61eb6d5d53e03ace0fe65cea653d98b
monosusio <ssanabriag@unbosque.edu.co>
2022-09-20 19:36:18 -0500

    creacion del git

---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,15 @@
+package co.edu.unbosque.controller;
+
+public class Controller {
+	
+	public Controller() {
+			
+		System.out.println("hola mundo");
+		
+	}
+	
+	public void inicializar() {
+		
+	}
+
+}
